92aa661a72ab1b3d6b1b0f4b4a978ca5c03aee98
Sattar <sattargh@aol.com>
2025-05-28 00:51:43 +0330

    added raw service layer for Transaction

---
+++ src/main/java/sat/basicbanksystem/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package sat.basicbanksystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sat.basicbanksystem.entity.Transaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+}
